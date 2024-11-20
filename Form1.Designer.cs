71008550d3f08897d94abb1dedc94ca425971402
Алексей Кипнис <kipnis.work@gmail.com>
2024-11-21 01:05:57 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace DiningPhilosophers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.philosopherLabel1 = new System.Windows.Forms.Label();
+            this.philosopherLabel2 = new System.Windows.Forms.Label();
+            this.philosopherLabel3 = new System.Windows.Forms.Label();
+            this.philosopherLabel4 = new System.Windows.Forms.Label();
+            this.philosopherLabel5 = new System.Windows.Forms.Label();
+            this.forkLabel5 = new System.Windows.Forms.Label();
+            this.forkLabel3 = new System.Windows.Forms.Label();
+            this.forkLabel2 = new System.Windows.Forms.Label();
+            this.forkLabel4 = new System.Windows.Forms.Label();
+            this.forkLabel1 = new System.Windows.Forms.Label();
+            this.mealsCountLabel1 = new System.Windows.Forms.Label();
+            this.mealsCountLabel2 = new System.Windows.Forms.Label();
+            this.mealsCountLabel3 = new System.Windows.Forms.Label();
+            this.mealsCountLabel4 = new System.Windows.Forms.Label();
+            this.mealsCountLabel5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // philosopherLabel1
+            // 
+            this.philosopherLabel1.AutoSize = true;
+            this.philosopherLabel1.Location = new System.Drawing.Point(422, 104);
+            this.philosopherLabel1.Name = "philosopherLabel1";
+            this.philosopherLabel1.Size = new System.Drawing.Size(68, 13);
+            this.philosopherLabel1.TabIndex = 0;
+            this.philosopherLabel1.Text = "Philosopher1";
+            // 
+            // philosopherLabel2
+            // 
+            this.philosopherLabel2.AutoSize = true;
+            this.philosopherLabel2.Location = new System.Drawing.Point(270, 173);
+            this.philosopherLabel2.Name = "philosopherLabel2";
+            this.philosopherLabel2.Size = new System.Drawing.Size(68, 13);
+            this.philosopherLabel2.TabIndex = 1;
+            this.philosopherLabel2.Text = "Philosopher2";
+            // 
+            // philosopherLabel3
+            // 
+            this.philosopherLabel3.AutoSize = true;
+            this.philosopherLabel3.Location = new System.Drawing.Point(324, 242);
+            this.philosopherLabel3.Name = "philosopherLabel3";
+            this.philosopherLabel3.Size = new System.Drawing.Size(68, 13);
+            this.philosopherLabel3.TabIndex = 2;
+            this.philosopherLabel3.Text = "Philosopher3";
+            // 
+            // philosopherLabel4
+            // 
+            this.philosopherLabel4.AutoSize = true;
+            this.philosopherLabel4.Location = new System.Drawing.Point(491, 242);
+            this.philosopherLabel4.Name = "philosopherLabel4";
+            this.philosopherLabel4.Size = new System.Drawing.Size(68, 13);
+            this.philosopherLabel4.TabIndex = 3;
+            this.philosopherLabel4.Text = "Philosopher4";
+            // 
+            // philosopherLabel5
+            // 
+            this.philosopherLabel5.AutoSize = true;
+            this.philosopherLabel5.Location = new System.Drawing.Point(563, 173);
+            this.philosopherLabel5.Name = "philosopherLabel5";
+            this.philosopherLabel5.Size = new System.Drawing.Size(68, 13);
+            this.philosopherLabel5.TabIndex = 4;
+            this.philosopherLabel5.Text = "Philosopher5";
+            // 
+            // forkLabel5
+            // 
+            this.forkLabel5.AutoSize = true;
+            this.forkLabel5.Location = new System.Drawing.Point(504, 200);
+            this.forkLabel5.Name = "forkLabel5";
+            this.forkLabel5.Size = new System.Drawing.Size(34, 13);
+            this.forkLabel5.TabIndex = 5;
+            this.forkLabel5.Text = "Fork5";
+            // 
+            // forkLabel3
+            // 
+            this.forkLabel3.AutoSize = true;
+            this.forkLabel3.Location = new System.Drawing.Point(349, 200);
+            this.forkLabel3.Name = "forkLabel3";
+            this.forkLabel3.Size = new System.Drawing.Size(34, 13);
+            this.forkLabel3.TabIndex = 6;
+            this.forkLabel3.Text = "Fork3";
+            // 
+            // forkLabel2
+            // 
+            this.forkLabel2.AutoSize = true;
+            this.forkLabel2.Location = new System.Drawing.Point(349, 136);
+            this.forkLabel2.Name = "forkLabel2";
+            this.forkLabel2.Size = new System.Drawing.Size(34, 13);
+            this.forkLabel2.TabIndex = 7;
+            this.forkLabel2.Text = "Fork2";
+            // 
+            // forkLabel4
+            // 
+            this.forkLabel4.AutoSize = true;
+            this.forkLabel4.Location = new System.Drawing.Point(437, 268);
+            this.forkLabel4.Name = "forkLabel4";
+            this.forkLabel4.Size = new System.Drawing.Size(34, 13);
+            this.forkLabel4.TabIndex = 8;
+            this.forkLabel4.Text = "Fork4";
+            // 
+            // forkLabel1
+            // 
+            this.forkLabel1.AutoSize = true;
+            this.forkLabel1.Location = new System.Drawing.Point(504, 136);
+            this.forkLabel1.Name = "forkLabel1";
+            this.forkLabel1.Size = new System.Drawing.Size(34, 13);
+            this.forkLabel1.TabIndex = 9;
+            this.forkLabel1.Text = "Fork1";
+            // 
+            // mealsCountLabel1
+            // 
+            this.mealsCountLabel1.AutoSize = true;
+            this.mealsCountLabel1.Location = new System.Drawing.Point(22, 31);
+            this.mealsCountLabel1.Name = "mealsCountLabel1";
+            this.mealsCountLabel1.Size = new System.Drawing.Size(35, 13);
+            this.mealsCountLabel1.TabIndex = 10;
+            this.mealsCountLabel1.Text = "label1";
+            // 
+            // mealsCountLabel2
+            // 
+            this.mealsCountLabel2.AutoSize = true;
+            this.mealsCountLabel2.Location = new System.Drawing.Point(22, 63);
+            this.mealsCountLabel2.Name = "mealsCountLabel2";
+            this.mealsCountLabel2.Size = new System.Drawing.Size(35, 13);
+            this.mealsCountLabel2.TabIndex = 11;
+            this.mealsCountLabel2.Text = "label2";
+            // 
+            // mealsCountLabel3
+            // 
+            this.mealsCountLabel3.AutoSize = true;
+            this.mealsCountLabel3.Location = new System.Drawing.Point(22, 100);
+            this.mealsCountLabel3.Name = "mealsCountLabel3";
+            this.mealsCountLabel3.Size = new System.Drawing.Size(35, 13);
+            this.mealsCountLabel3.TabIndex = 12;
+            this.mealsCountLabel3.Text = "label3";
+            // 
+            // mealsCountLabel4
+            // 
+            this.mealsCountLabel4.AutoSize = true;
+            this.mealsCountLabel4.Location = new System.Drawing.Point(22, 136);
+            this.mealsCountLabel4.Name = "mealsCountLabel4";
+            this.mealsCountLabel4.Size = new System.Drawing.Size(35, 13);
+            this.mealsCountLabel4.TabIndex = 13;
+            this.mealsCountLabel4.Text = "label4";
+            // 
+            // mealsCountLabel5
+            // 
+            this.mealsCountLabel5.AutoSize = true;
+            this.mealsCountLabel5.Location = new System.Drawing.Point(22, 173);
+            this.mealsCountLabel5.Name = "mealsCountLabel5";
+            this.mealsCountLabel5.Size = new System.Drawing.Size(35, 13);
+            this.mealsCountLabel5.TabIndex = 14;
+            this.mealsCountLabel5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mealsCountLabel5);
+            this.Controls.Add(this.mealsCountLabel4);
+            this.Controls.Add(this.mealsCountLabel3);
+            this.Controls.Add(this.mealsCountLabel2);
+            this.Controls.Add(this.mealsCountLabel1);
+            this.Controls.Add(this.forkLabel1);
+            this.Controls.Add(this.forkLabel4);
+            this.Controls.Add(this.forkLabel2);
+            this.Controls.Add(this.forkLabel3);
+            this.Controls.Add(this.forkLabel5);
+            this.Controls.Add(this.philosopherLabel5);
+            this.Controls.Add(this.philosopherLabel4);
+            this.Controls.Add(this.philosopherLabel3);
+            this.Controls.Add(this.philosopherLabel2);
+            this.Controls.Add(this.philosopherLabel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label philosopherLabel1;
+        private System.Windows.Forms.Label philosopherLabel2;
+        private System.Windows.Forms.Label philosopherLabel3;
+        private System.Windows.Forms.Label philosopherLabel4;
+        private System.Windows.Forms.Label philosopherLabel5;
+        private System.Windows.Forms.Label forkLabel5;
+        private System.Windows.Forms.Label forkLabel3;
+        private System.Windows.Forms.Label forkLabel2;
+        private System.Windows.Forms.Label forkLabel4;
+        private System.Windows.Forms.Label forkLabel1;
+        private System.Windows.Forms.Label mealsCountLabel1;
+        private System.Windows.Forms.Label mealsCountLabel2;
+        private System.Windows.Forms.Label mealsCountLabel3;
+        private System.Windows.Forms.Label mealsCountLabel4;
+        private System.Windows.Forms.Label mealsCountLabel5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DiningPhilosophers
+{
+    public partial class Form1 : Form
+    {
+        private const int PhilosopherCount = 5;
+        private Semaphore waiter = new Semaphore(4, 4); // Семафор для предотвращения дедлока
+        private Mutex[] forks = new Mutex[PhilosopherCount]; // Массив мьютексов для вилок
+        private Thread[] philosopherThreads = new Thread[PhilosopherCount];
+        private int[] mealsCount = new int[PhilosopherCount]; // Количество приемов пищи для каждого философа
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Инициализируем мьютексы для вилок
+            for (int i = 0; i < PhilosopherCount; i++)
+            {
+                forks[i] = new Mutex(); // Каждый философ получает мьютекс для своей вилки
+            }
+
+            // Создаем потоки для философов
+            for (int i = 0; i < PhilosopherCount; i++)
+            {
+                int philosopherIndex = i; // Локальная переменная для замыкания в потоке
+                philosopherThreads[i] = new Thread(() => Philosopher(philosopherIndex));
+                philosopherThreads[i].Start();
+            }
+        }
+
+        private void Philosopher(int philosopherIndex)
+        {
+            while (true)
+            {
+                Think(philosopherIndex);
+                Eat(philosopherIndex);
+            }
+        }
+
+        private void Think(int philosopherIndex)
+        {
+            // Философ думает
+            UpdatePhilosopherState(philosopherIndex, "Thinking...");
+            Thread.Sleep( new Random().Next(1000, 5000)); // Философ думает случайное время
+        }
+
+        private void Eat(int philosopherIndex)
+        {
+            int leftFork = philosopherIndex;
+            int rightFork = (philosopherIndex + 1) % PhilosopherCount;
+
+            waiter.WaitOne(); // Ожидаем, чтобы избежать дедлока
+
+            // Попытка взять вилки
+            bool gotLeftFork = false;
+            bool gotRightFork = false;
+
+            try
+            {
+                // Философ пытается взять левую вилку
+                gotLeftFork = forks[leftFork].WaitOne(1000); // Ожидаем мьютекс на вилку с тайм-аутом
+                if (gotLeftFork)
+                {
+                    // Философ пытается взять правую вилку
+                    gotRightFork = forks[rightFork].WaitOne(1000); // Ожидаем мьютекс на вилку с тайм-аутом
+                }
+
+                if (gotLeftFork && gotRightFork)
+                {
+                    // Если обе вилки захвачены, философ ест
+                    UpdateForkState(leftFork, "Taken");
+                    UpdateForkState(rightFork, "Taken");
+
+                    UpdatePhilosopherState(philosopherIndex, "Eating...");
+                    mealsCount[philosopherIndex]++;
+                    UpdateMealsCount(philosopherIndex);
+
+                    Thread.Sleep(new Random().Next(1000, 5000)); // Философ ест случайное время
+
+                    // Философ закончил есть, кладет вилки
+                    forks[leftFork].ReleaseMutex(); // Освобождаем левую вилку
+                    forks[rightFork].ReleaseMutex(); // Освобождаем правую вилку
+
+                    UpdateForkState(leftFork, "Free");
+                    UpdateForkState(rightFork, "Free");
+                }
+            }
+            finally
+            {
+                // Если философ смог захватить только одну вилку, освобождаем захваченную
+                if (gotLeftFork && !gotRightFork)
+                {
+                    forks[leftFork].ReleaseMutex(); // Освобождаем левую вилку
+                }
+                // Если философ не смог захватить обе вилки, он будет снова пытаться
+            }
+
+            waiter.Release(); // Освобождаем семафор
+        }
+
+        private void UpdatePhilosopherState(int philosopherIndex, string state)
+        {
+            // Обновляем состояние философа
+            if (InvokeRequired)
+            {
+                BeginInvoke((MethodInvoker)(() =>
+                {
+                    switch (philosopherIndex)
+                    {
+                        case 0: philosopherLabel1.Text = $"{state}"; break;
+                        case 1: philosopherLabel2.Text = $"{state}"; break;
+                        case 2: philosopherLabel3.Text = $"{state}"; break;
+                        case 3: philosopherLabel4.Text = $"{state}"; break;
+                        case 4: philosopherLabel5.Text = $"{state}"; break;
+                    }
+                }));
+            }
+        }
+
+        private void UpdateForkState(int forkIndex, string state)
+        {
+            // Обновляем состояние вилки
+            if (InvokeRequired)
+            {
+                BeginInvoke((MethodInvoker)(() =>
+                {
+                    switch (forkIndex)
+                    {
+                        case 0: forkLabel1.Text = $"{state}"; break;
+                        case 1: forkLabel2.Text = $"{state}"; break;
+                        case 2: forkLabel3.Text = $"{state}"; break;
+                        case 3: forkLabel4.Text = $"{state}"; break;
+                        case 4: forkLabel5.Text = $"{state}"; break;
+                    }
+                }));
+            }
+        }
+
+        private void UpdateMealsCount(int philosopherIndex)
+        {
+            // Обновляем количество приемов пищи
+            if (InvokeRequired)
+            {
+                BeginInvoke((MethodInvoker)(() =>
+                {
+                    switch (philosopherIndex)
+                    {
+                        case 0: mealsCountLabel1.Text = $"Meals: {mealsCount[0]}"; break;
+                        case 1: mealsCountLabel2.Text = $"Meals: {mealsCount[1]}"; break;
+                        case 2: mealsCountLabel3.Text = $"Meals: {mealsCount[2]}"; break;
+                        case 3: mealsCountLabel4.Text = $"Meals: {mealsCount[3]}"; break;
+                        case 4: mealsCountLabel5.Text = $"Meals: {mealsCount[4]}"; break;
+                    }
+                }));
+            }
+        }
+    }
+}
